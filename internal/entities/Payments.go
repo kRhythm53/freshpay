18e39bd8072e8e8208f6f41607813560d4f44fbb
kshitij-nawandar9 <87687615+kshitij-nawandar9@users.noreply.github.com>
2021-08-03 12:19:57 +0530

    updated payments entities

--- internal/entities/Payments.go
+++ internal/entities/Payments.go
@@ -4,6 +4,7 @@ import "github.com/jinzhu/gorm"
 
 type Payments struct {
 	gorm.Model
+	Id string `json:"id",gorm:"primaryKey"`
 	Amount uint `json:"amount"`
 	Currency string `json:"currency"`
 	SourceId string `json:"source_id"`
--- internal/entities/Transactions.go
+++ internal/entities/Transactions.go
@@ -4,17 +4,18 @@ import "github.com/jinzhu/gorm"
 
 type Transactions struct {
 	gorm.Model
+	Id string `json:"id",gorm:"primaryKey"`
 	Amount uint `json:"amount"`
 	Currency string `json:"currency"`
 	SourceId string `json:"source_id"`
 	DestinationId string `json:"destination_id"`
 	Type string `json:"type"`
 	Status     string `json:"status"`
-	PaymentId uint
+	PaymentId string `json:"payment_id"`
 	Payments Payments
 }
 
-func (b *Payments) TableName() string {
-	return "payments"
+func (b *Transactions) TableName() string {
+	return "Transactions"
 }
 //added a comment
