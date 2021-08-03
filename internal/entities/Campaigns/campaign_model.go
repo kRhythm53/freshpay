1278efa689ae9c90049d22320062d2961c270cd5
Rhythm <35167328+kRhythm@users.noreply.github.com>
2021-08-03 12:37:55 +0530

    changed the type of start and end time

--- internal/entities/Campaigns/campaign_model.go
+++ internal/entities/Campaigns/campaign_model.go
@@ -7,8 +7,8 @@ type Campaign struct {
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time ``json:"updated_at"
 	CampaignType 				string `json:"name"`
-	StartTime 			string `json:"start_time"`
-	EndTime 			string `json:"end_time"`
+	StartTime 			time.Time `json:"start_time"`
+	EndTime 			time.Time `json:"end_time"`
 	Count 				string `json:"count"`
 	TransactionNumber  string `json:"transaction_number"`
 	IsActive 			string `json:"is_active"`
