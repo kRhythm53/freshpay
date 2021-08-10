c8117b2db7c0fd2acab21c7b06c1a4619dc2ebb3
Akhilesh-Sirohi <akhileshsirohii@gmail.com>
2021-08-10 11:46:21 +0530

    Update routes.go

--- routes/routes.go
+++ routes/routes.go
@@ -1,7 +1,8 @@
 package routes
 
 import (
-	campaigns "github.com/freshpay/internal/controllers/campaign"
+	"github.com/freshpay/internal/controllers/admin_management"
+	"github.com/freshpay/internal/controllers/otp_verification"
 	"github.com/freshpay/internal/controllers/payments"
 	"github.com/freshpay/internal/controllers/user_management"
 	"github.com/freshpay/middleware"
@@ -22,6 +23,7 @@ func SetupRouter() *gin.Engine {
 	grp2:= r.Group("/users")
 	{
 		grp2.POST("signup",user_management.SignUp)
+		grp2.POST("signup/otp/verification",otp_verification.VerifyOTPUser)
 		grp2.POST("signin",user_management.LoginByPassword)
 		grp2.POST("bankaccount",user_management.AddBankAccount)
 		grp2.GET("bankaccounts",user_management.GetAllBankAccountByUserId)
@@ -31,18 +33,20 @@ func SetupRouter() *gin.Engine {
 
 		grp2.GET("balance",user_management.GetWalletBalance)
 	}
-	grp5:= r.Group("/admin")
-	{
-		grp5.POST("signup",admin_management.SignUp)
-		grp5.POST("signup/otp/verification",otp_verification.VerifyOTPAdmin)
-		grp5.POST("signin",admin_management.LoginByPassword)
-	}
+	
 	grp3:= r.Group("/campaigns")
 	{
 		grp3.POST("/",campaigns.CreateCampaign)
 		grp3.GET("/",campaigns.GetCampaign)
 		grp3.GET("/:campaign_id",campaigns.GetCampaignByID)
 		grp3.PATCH("/:campaign_id",campaigns.UpdateCampaign)
+	}
+	
+	grp5:= r.Group("/admin")
+	{
+		grp5.POST("signup",admin_management.SignUp)
+		grp5.POST("signup/otp/verification",otp_verification.VerifyOTPAdmin)
+		grp5.POST("signin",admin_management.LoginByPassword)
 	}
 	return r
 }
